11fc903911f71c7116641c4a038b1bdfb53814c3
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-05-25 16:15:42 +0300

    addEventListener + api

--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@
 import fetchData from './js/pixabay-api.js';
 // // import moduleName from './js/render-functions.js';
 
+const searchForm = document.querySelector('.form');
 
-fetchData('nature').then(images => {
-  console.log(images);
-}).catch(error => {
-  console.error('Error fetching images:', error);
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const query = event.target.elements["search-text"].value.trim();
+
+  console.log('Search query:', query);
+
+  fetchData(query).then(images => {
+    console.log(images);
+  }).catch(error => {
+    console.error('Error fetching images:', error);
+  });
 });
