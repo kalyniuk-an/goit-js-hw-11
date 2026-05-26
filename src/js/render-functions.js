2d1ed02d4694105ddd38a62bab3f889932fee6a4
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-05-26 16:01:26 +0300

    markings

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,18 +3,26 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const gallery = document.querySelector(".gallery");
 
+function createGalleryItem({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
+  return `
+    <li class="gallery-item">
+      <a href="${largeImageURL}" data-lightbox="gallery">
+        <img src="${webformatURL}" alt="${tags}" />
+      </a>
+      <div class="details">
+        <p><strong>Likes:</strong> ${likes}</p>
+        <p><strong>Views:</strong> ${views}</p>
+        <p><strong>Comments:</strong> ${comments}</p>
+        <p><strong>Downloads:</strong> ${downloads}</p>
+      </div>
+    </li>
+  `;
+}
+
 export function createGallery(items) {
   
   const markup = items
-    .map(
-      ({ webformatURL, largeImageURL, tags }) => `
-        <li class="gallery-item">
-          <a href="${largeImageURL}" data-lightbox="gallery">
-            <img src="${webformatURL}" alt="${tags}" />
-          </a>
-        </li>
-      `
-    )
+    .map(createGalleryItem)
     .join("");
   gallery.innerHTML = markup;
 }
